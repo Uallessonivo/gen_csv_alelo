92c730f8ad0ffe37911dd6dbec3153d98a176fd8
Uallesson Nunes Ivo <uallessons@gmail.com>
2022-06-16 20:44:55 -0300

    Database and crud impl

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': {
-      'endOfLine': 'auto',
-    }
+    '@typescript-eslint/no-explicit-any': 'off'
   },
 };
